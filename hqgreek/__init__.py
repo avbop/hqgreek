47817e6f63bfd5cb017967e594c980e947c38ba4
Anthony VanBerkum <avbop@users.noreply.github.com>
2016-05-29 09:39:27 -0400

    More structure stuff.

---
+++ hqgreek/__init__.py
@@ -0,0 +1,3 @@
+from .classes import Verb
+
+__all__ = ['Verb']
---
+++ hqvocab/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+import hqgreek
+
+from .omega_verbs import paideuw
+
+__all__ = ['paideuw']
