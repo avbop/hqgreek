699291704c5b91694766aa4ec61415a17c6f636f
Anthony VanBerkum <avbop@users.noreply.github.com>
2016-07-16 16:17:09 -0400

    Better testing of conjugations.

---
+++ tests/test_omega.py
@@ -0,0 +1,41 @@
+from context import hqgreek
+from context import hqvocab
+
+from hqgreek.conjugations import omega
+from hqgreek.morphology import *
+
+def test_omega_present():
+  data = (omega.present, 'παιδευ')
+  assert data[0](data, [FIRST, SINGULAR, PRESENT, INDICATIVE, ACTIVE]) == ['παιδεύω']
+  assert data[0](data, [PLURAL, SECOND, PRESENT, INDICATIVE, ACTIVE]) == ['παιδεύετε']
+  data = (omega.present, 'λυ-')
+  assert data[0](data, [FIRST, SINGULAR, PRESENT, INDICATIVE, ACTIVE]) == ['λύω']
+  assert data[0](data, [PRESENT, SINGULAR, THIRD, INDICATIVE, ACTIVE]) == ['λύει']
+
+def test_hqvocab():
+  assert hqvocab.paideuw.conjugate([FIRST, SINGULAR, PRESENT, INDICATIVE,
+    ACTIVE]) == ['παιδεύω']
+  assert hqvocab.luw.conjugate([FIRST, SINGULAR, PRESENT, INDICATIVE,
+    ACTIVE]) == ['λύω']
+
+def test_many_forms():
+  assert hqvocab.paideuw.many_forms(([FIRST, SECOND], [PRESENT], [INDICATIVE],
+      [ACTIVE], [SINGULAR, PLURAL])) ==\
+      [['παιδεύω'], ['παιδεύομεν'], ['παιδεύεις'], ['παιδεύετε']]
+  assert hqvocab.paideuw.many_forms(([FIRST, SECOND], [PRESENT], [INDICATIVE],
+      [ACTIVE], [SINGULAR, PLURAL]), include_translations=True) ==\
+      ([['παιδεύω'], ['παιδεύομεν'], ['παιδεύεις'], ['παιδεύετε']],
+      ['first singular present indicative active',
+       'first plural present indicative active',
+       'second singular present indicative active',
+       'second plural present indicative active'])
+  assert hqvocab.luw.many_forms(([FIRST, SECOND], [PRESENT], [INDICATIVE],
+      [ACTIVE], [SINGULAR, PLURAL])) ==\
+      [['λύω'], ['λύομεν'], ['λύεις'], ['λύετε']]
+  assert hqvocab.luw.many_forms(([FIRST, SECOND], [PRESENT], [INDICATIVE],
+      [ACTIVE], [SINGULAR, PLURAL]), include_translations=True) ==\
+      ([['λύω'], ['λύομεν'], ['λύεις'], ['λύετε']],
+      ['first singular present indicative active',
+       'first plural present indicative active',
+       'second singular present indicative active',
+       'second plural present indicative active'])
