281898210f1661ba453872c32c7fd8eeca54b78e
Anthony VanBerkum <avbop@users.noreply.github.com>
2016-07-16 16:31:49 -0400

    Add omega present middle/passive.

--- hqgreek/conjugations/omega.py
+++ hqgreek/conjugations/omega.py
@@ -4,7 +4,15 @@ from hqgreek.morphology import *
 def present(args, morph):
   _, root = args
   words = []
-  if set([FIRST, SINGULAR, PRESENT, INDICATIVE, ACTIVE]) == set(morph):
+  # Turn requests for middle voice into passive voice, since the forms are the
+  # same.
+  if MIDDLE in morph:
+    new_morph = morph.copy()
+    new_morph.remove(MIDDLE)
+    new_morph.append(PASSIVE)
+    return present(args, new_morph)
+  # Active indicative.
+  elif set([FIRST, SINGULAR, PRESENT, INDICATIVE, ACTIVE]) == set(morph):
     words.append(root + 'ω')
   elif set([SECOND, SINGULAR, PRESENT, INDICATIVE, ACTIVE]) == set(morph):
     words.append(root + 'εις')
@@ -17,6 +25,20 @@ def present(args, morph):
   elif set([THIRD, PLURAL, PRESENT, INDICATIVE, ACTIVE]) == set(morph):
     words.append(root + 'ουσιν')
     words.append(root + 'ουσι')
+  # Middle/passive indicative.
+  elif set([FIRST, SINGULAR, PRESENT, INDICATIVE, PASSIVE]) == set(morph):
+    words.append(root + 'ομαι')
+  elif set([SECOND, SINGULAR, PRESENT, INDICATIVE, PASSIVE]) == set(morph):
+    words.append(root + 'ει')
+    words.append(root + 'ῃ')
+  elif set([THIRD, SINGULAR, PRESENT, INDICATIVE, PASSIVE]) == set(morph):
+    words.append(root + 'εται')
+  elif set([FIRST, PLURAL, PRESENT, INDICATIVE, PASSIVE]) == set(morph):
+    words.append(root + 'ομεθα')
+  elif set([SECOND, PLURAL, PRESENT, INDICATIVE, PASSIVE]) == set(morph):
+    words.append(root + 'εσθε')
+  elif set([THIRD, PLURAL, PRESENT, INDICATIVE, PASSIVE]) == set(morph):
+    words.append(root + 'ονται')
   else:
     raise InvalidMorphologyError
   for i, word in enumerate(words):
--- tests/test_omega.py
+++ tests/test_omega.py
@@ -4,13 +4,25 @@ from context import hqvocab
 from hqgreek.conjugations import omega
 from hqgreek.morphology import *
 
-def test_omega_present():
+def test_omega_present_active():
   data = (omega.present, 'παιδευ')
   assert data[0](data, [FIRST, SINGULAR, PRESENT, INDICATIVE, ACTIVE]) == ['παιδεύω']
   assert data[0](data, [PLURAL, SECOND, PRESENT, INDICATIVE, ACTIVE]) == ['παιδεύετε']
   data = (omega.present, 'λυ-')
   assert data[0](data, [FIRST, SINGULAR, PRESENT, INDICATIVE, ACTIVE]) == ['λύω']
   assert data[0](data, [PRESENT, SINGULAR, THIRD, INDICATIVE, ACTIVE]) == ['λύει']
+
+def test_omega_present_middle_passive():
+  data = (omega.present, 'παιδευ')
+  assert data[0](data, [FIRST, SINGULAR, PRESENT, INDICATIVE, PASSIVE]) == ['παιδεύομαι']
+  assert data[0](data, [PLURAL, SECOND, PRESENT, INDICATIVE, PASSIVE]) == ['παιδεύεσθε']
+  assert data[0](data, [FIRST, SINGULAR, PRESENT, INDICATIVE, MIDDLE]) == ['παιδεύομαι']
+  assert data[0](data, [PLURAL, SECOND, PRESENT, INDICATIVE, MIDDLE]) == ['παιδεύεσθε']
+  data = (omega.present, 'λυ-')
+  assert data[0](data, [FIRST, SINGULAR, PRESENT, INDICATIVE, PASSIVE]) == ['λύομαι']
+  assert data[0](data, [PRESENT, SINGULAR, THIRD, INDICATIVE, PASSIVE]) == ['λύεται']
+  assert data[0](data, [FIRST, SINGULAR, PRESENT, INDICATIVE, MIDDLE]) == ['λύομαι']
+  assert data[0](data, [PRESENT, SINGULAR, THIRD, INDICATIVE, MIDDLE]) == ['λύεται']
 
 def test_hqvocab():
   assert hqvocab.paideuw.conjugate([FIRST, SINGULAR, PRESENT, INDICATIVE,
